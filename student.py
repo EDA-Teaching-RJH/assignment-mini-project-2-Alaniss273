cac11931273e1c03483da21990c2477afea59542
Alaniss273 <ah2195@kent.ac.uk>
2026-03-12 13:41:47 +0000

    reflection (8) - lecture 9, also capitalised 'p' for "class Person"

--- student.py
+++ student.py
@@ -2,12 +2,12 @@
 from grade import calculate_average, assign_letter_grade
 
 #base class
-class person:
+class Person:
     def __init__(self, name):
         self.name = name
 
 #student class inherits from person
-class Student(person):
+class Student(Person):
     def __init__(self, student_id, name, grades):
         super().__init__(name)
         self.student_id = student_id
